6b3186428b2dd8032703a847d4ea7f64acf0760b
moisutsu <moisutsu@gmail.com>
2020-08-09 18:23:10 +0900

    Add function to convert clipboard image to image::DynamicImage

--- src/imboard.rs
+++ src/imboard.rs
@@ -1,16 +1,39 @@
 extern crate anyhow;
+extern crate image;
 extern crate tempfile;
+extern crate tokio;
 
 use anyhow::Result;
+use image::io::Reader;
+use image::DynamicImage;
 use tempfile::NamedTempFile;
+use tokio::process::Command;
 
 #[cfg(target_os = "macos")]
-pub fn copy_image_from_clipboard() -> Result<()> {
-    let file = NamedTempFile::new()?;
-    let file_name = format!("{:?}", file.path());
-    println!("{}", file_name);
-    file.close()?;
-    Ok(())
+pub async fn copy_image_from_clipboard() -> Result<DynamicImage> {
+    // Create a temporary file for storing clipboard image
+    let temp_file = NamedTempFile::new()?;
+    let temp_file_name = temp_file.path().to_str().unwrap();
+
+    //  Copy clipboard image to a temporary file
+    let script_to_copy = &format!(
+        "write (the clipboard as «class PNGf») to (open for access \"{}\" with write permission)",
+        temp_file_name
+    );
+    Command::new("osascript")
+        .args(&["-e", script_to_copy])
+        .spawn()?
+        .await?;
+
+    // Convert image from temporary file to <image::DynamicImage>
+    let img = Reader::open(temp_file_name)?
+        .with_guessed_format()?
+        .decode()?;
+
+    // Delete temporary file
+    temp_file.close()?;
+
+    Ok(img)
 }
 
 #[cfg(not(target_os = "macos"))]
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 
-fn main() -> Result<()> {
-    imboard::copy_image_from_clipboard()?;
+#[tokio::main]
+async fn main() -> Result<()> {
+    let img = imboard::copy_image_from_clipboard().await?;
+    img.to_rgba().save("sample.png").unwrap();
     Ok(())
 }
