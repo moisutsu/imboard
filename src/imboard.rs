0cd0e5ccb4d13869a02dd69c3aeceee5363446ef
moisutsu <moisutsu@gmail.com>
2020-08-09 19:55:07 +0900

    Add function to copy image::DynamicImage to the clipboard

--- src/imboard.rs
+++ src/imboard.rs
@@ -6,6 +6,7 @@ extern crate tokio;
 use anyhow::Result;
 use image::io::Reader;
 use image::DynamicImage;
+use image::ImageFormat::Png;
 use tempfile::NamedTempFile;
 use tokio::process::Command;
 
@@ -39,5 +40,36 @@ pub async fn copy_image_from_clipboard() -> Result<DynamicImage> {
 
 #[cfg(not(target_os = "macos"))]
 pub fn copy_image_from_clipboard() -> Result<()> {
+    Ok(())
+}
+
+/// A `copy_image_to_clipboard` copies the argument img to the clipboard
+#[cfg(target_os = "macos")]
+pub async fn copy_image_to_clipboard(img: DynamicImage) -> Result<()> {
+    // Create a temporary file to save image to the clipboard
+    let temp_file = NamedTempFile::new()?;
+    let temp_file_name = temp_file.path().to_str().unwrap();
+
+    // Save the argument img to a temporary file as png
+    img.save_with_format(temp_file_name, Png).unwrap();
+
+    // Copy temporary file image to the clipboard
+    let script_to_copy = &format!(
+        "set the clipboard to (read \"{}\" as TIFF picture)",
+        temp_file_name
+    );
+    Command::new("osascript")
+        .args(&["-e", script_to_copy])
+        .spawn()?
+        .await?;
+
+    // Delete temporary file
+    temp_file.close()?;
+
+    Ok(())
+}
+
+#[cfg(not(target_os = "macos"))]
+pub fn copy_image_to_clipboard() -> Result<()> {
     Ok(())
 }
